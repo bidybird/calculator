5842441e0efcf9466fd16f75989de13de080385f
Jonathan Shovak <shovakitup@gmail.com>
2022-10-19 13:09:55 -0500

    added onclick to the buttons that have the basic command of adding a piece of string to the variable screen

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let screen = "";
+document.querySelector("#display").textContent = screen;
+
 let solution = 0;
-let display = "4 / 3";
+let display = "4 / 3"; //screen;
 displayArray = display.split(" ");
 console.log(displayArray);
 
@@ -45,3 +48,45 @@ function divide(a, b) {
   }
   solution = a / b;
 }
+
+const btn1 = document.querySelector("#buttonOne");
+btn1.onclick = () => (screen = screen + "1");
+
+const btn2 = document.querySelector("#buttonTwo");
+btn2.onclick = () => (screen = screen + "2");
+
+const btn3 = document.querySelector("#buttonThree");
+btn3.onclick = () => (screen = screen + "3");
+
+const btn4 = document.querySelector("#buttonFour");
+btn4.onclick = () => (screen = screen + "4");
+
+const btn5 = document.querySelector("#buttonFive");
+btn5.onclick = () => (screen = screen + "5");
+
+const btn6 = document.querySelector("#buttonSix");
+btn6.onclick = () => (screen = screen + "6");
+
+const btn7 = document.querySelector("#buttonSeven");
+btn7.onclick = () => (screen = screen + "7");
+
+const btn8 = document.querySelector("#buttonEight");
+btn8.onclick = () => (screen = screen + "8");
+
+const btn9 = document.querySelector("#buttonNine");
+btn9.onclick = () => (screen = screen + "9");
+
+const btn0 = document.querySelector("#buttonZero");
+btn0.onclick = () => (screen = screen + "0");
+
+const btnAdd = document.querySelector("#buttonAdd");
+btnAdd.onclick = () => (screen = screen + " + ");
+
+const btnSubtract = document.querySelector("#buttonSubtract");
+btnSubtract.onclick = () => (screen = screen + " - ");
+
+const btnMultiply = document.querySelector("#buttonMultiply");
+btnMultiply.onclick = () => (screen = screen + " X ");
+
+const btnDivide = document.querySelector("#buttonDivide");
+btnDivide.onclick = () => (screen = screen + " / ");
